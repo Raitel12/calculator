da5ad722e9c108b155e5649fd3aaafcb80efa7eb
Илья <unitsin@bk.ru>
2024-02-19 23:10:55 +0300

    Рабочая версия калькулятора

---
+++ src/Value.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Value {
+    public int value;
+    public String valueRim;
+    public boolean rim;
+
+    public Value(String valueRim, int value, Boolean rim)
+    {
+        this.rim = rim;
+        this.valueRim = valueRim;
+        this.value = value;
+    }
+}
